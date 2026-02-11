6bfa9ea7f425262c5740b26cc84c804d36700be1
Viet Khoi <khoinv2006@gmail.com>
2026-02-11 14:52:55 +0700

    Add design and implmentation of class Interpreter (not tested)

--- struct.py
+++ struct.py
@@ -50,9 +50,91 @@ class List:
         else:
             self.array.pop(index)
 
+    def renameTask(self, index, name):
+        if (index >= len(self.array)):
+            raise Exception("List :: renametask : Incorrect index!")
+        else:
+            self.array[index].setName(name)
+
+
     def __str__(self):
         li  = []
         li.append(f"TO-DO-LIST: {self.name}")
         for i in range (len(self.array)):
             li.append(f"{i}. {self.array[i].getContent()}")
         return "\n".join(li)
+
+
+operator = {":add" : 0, ":remove" : 1, ":tick" : 2, ":rename" : 3, "setFileName" : 4, ":open" : 5, ":close" : 6}
+
+
+class Interpreter:
+
+    def __init__(self):
+        self.to_do_list = List("")
+        self.fileName = None
+        
+    def parser(self, command):
+        parse = command.split(" ")
+        op = parse[0]
+        parse.pop(0)
+        execute(op, parse)
+
+    def execute(self, op, li):
+        val = operator.get(op)
+        match val:
+            case 0: #Add a task
+                self.to_do_list.addTask(" ".join.li)
+            case 1: #Remove a task
+                s = "".join.li
+                if (s.isnumeric()):
+                    self.to_do_list.removeTask(int(s))
+                else:
+                    print("Invalid operand to remove the task!")
+            case 2: #Tick off a Task
+                s = "".join.li
+                if (s.isnumeric()):
+                    self.to_do_list.tickOffTask(int(s))
+            case 3: #Rename Task
+                idx = li[0]
+                li.pop(0)
+                s = " ".join(li)
+                self.to_do_list.renameTask(idx, s)
+            case 4: #setListName
+                s = " ".join.li
+                self.to_do_list.setName(s)
+            case 5:   #Open file
+                location = " ".join.li + ".txt"
+                self.fileName = location
+                with open(location, 'r') as file:
+                    counter = 0
+                    for line in file:
+                        if (counter == 0):
+                            self.to_do_list.setName(line.strip())
+                            counter += 1
+                        else:
+                            state = line[0: line.find(" ")]
+                            content = line[line.find(" ") + 1:]
+                            if (state == "0"):
+                                state = False
+                            else:
+                                state = True
+                            task = Task(content)
+                            task.setStatus(status)
+                            self.to_do_list.append(task)
+
+            case 6: #Close a file
+                fileName = self.fileName
+                with open(fileName, 'w') as file:
+                    file.write(self.to_do_list.getName() + "\n")
+                    for task in self.to_do_list:
+                        if (task.getStatus() == False):
+                            state = 0
+                        else:
+                            state = 1
+                        file.write(f"{{state} {task.getName()}\n")
+
+            case None:
+                print("Unsupported command")
+
+
